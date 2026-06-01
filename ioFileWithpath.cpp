786e638b36b160525b7f25a51e38392ebff225c2
nababanalmaidah <nababanalmaidah@gmail.com>
2026-06-01 21:34:21 +0700

    menambahkan input nama file dari pengguna

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -3,3 +3,16 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+
+    
