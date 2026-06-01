39f54a94a05beb57b47cf48fa0902e63e9fc6bd4
nababanalmaidah <nababanalmaidah@gmail.com>
2026-06-01 18:30:16 +0700

    menambahkan libary dan namespace

---
+++ ExceptionStandart.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Awal program" << endl; 
+    try
+    {
+        array <int, 3> data = { 1, 2, 3 };
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e)
+    {
+
+        cout << e.what() << endl;
+    }
+    cout << "Baris program terakhir"  << endl;
+    return 0;
+
+}
